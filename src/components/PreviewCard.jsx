9987301ce2170da0a33e3f357e8d922b9b38468e
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-07-14 21:28:44 +0100

    corrected the vewprt

--- src/components/PreviewCard.jsx
+++ src/components/PreviewCard.jsx
@@ -5,24 +5,24 @@ import LuxuryIcon from '../assets/images/icon-luxury.svg'
 
 function PreviewCard() {
   return (
-    <div className='w-[280px] h-{3265px]'>
-        <div className='card-one h-[380px]'>
+    <div className='w-[280px] h-[620px]'>
+        <div className='card-one'>
             <img src={SadanIcon} alt="" />
             <p className='font-bold text-2xl my-6'>SADANS</p>
             <p className='text-xs font-thin w-[28ch] leading-6'>Choose a sadan for it affordability and excellent fuel economy. Ideal for crusing in the city or for your next road trip.</p>
-            <button className='bg-white text-[#e38826] p-3 my-6 rounded-3xl px-6'>Learn More</button>
+            <button className='bg-white text-[#e38826] p-3 mt-4 rounded-3xl px-6 text-xs'>Learn More</button>
         </div>
-        <div className='card-two h-[420px]'>
+        <div className='card-two'>
             <img src={SuvIcon} alt="" />
-            <p>SUVS</p>
-            <p>Take an SUV for its spacious interior, power and versatitlity. Perfect for your family vacation and off-road adventures. </p>
-            <button>Learn More</button>
+            <p className='font-bold text-2xl my-6'>SUVS</p>
+            <p className='text-xs font-thin w-[28ch] leading-6'>Take an SUV for its spacious interior, power and versatitlity. Perfect for your family vacation and off-road adventures. </p>
+            <button className='bg-white text-[#e38826] p-3 mt-4 rounded-3xl px-6 text-xs'>Learn More</button>
         </div>
-        <div className='card-three h-[420px]'>
+        <div className='card-three'>
             <img src={LuxuryIcon} alt="" />
-            <p>Luxury</p>
-            <p>Cruise in the best car brands without bloated price. Enjoy the enhanced comfort of a luxury rental and arrive in style.</p>
-            <button>Learn More</button>
+            <p className='font-bold text-2xl my-6'>Luxury</p>
+            <p className='text-xs font-thin w-[28ch] leading-6'>Cruise in the best car brands without bloated price. Enjoy the enhanced comfort of a luxury rental and arrive in style.</p>
+            <button className='bg-white text-[#e38826] p-3 mt-4 rounded-3xl px-6 text-xs'>Learn More</button>
         </div>
     </div>
   )
